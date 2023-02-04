b42272f9815e684ad639f0901cfea60390bb6104
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2023-02-04 12:52:00 +0100

    chap1: reorganize the bitcoin chapeter and write about transaction
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- sections/.auctex-auto/cap_2.el
+++ sections/.auctex-auto/cap_2.el
@@ -3,8 +3,6 @@
  (lambda ()
    (LaTeX-add-labels
     "sec:basics"
-    "sec:htlc_intro")
-   (LaTeX-add-environments
-    '("example" LaTeX-env-args ["argument"] 0)))
+    "sec:htlc_intro"))
  :latex)
 
